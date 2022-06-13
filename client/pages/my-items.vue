2bff5aba947b80fd201568c76960100902857a2f
Chris Rowe <100010401+Rowe2ry@users.noreply.github.com>
2022-06-13 16:52:56 -0400

    finished application

--- client/pages/my-items.vue
+++ client/pages/my-items.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
-    My Items Page
+    <MyItem
+      v-for="myItem in myRentals"
+      :key="myItem.id"
+      :item="myItem"
+    />
   </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
   export default {
-    layout: 'no-nav'
+    layout: 'no-nav',
+    computed: {
+      ...mapState(['myRentals'])
+    }
   }
 </script>
 
